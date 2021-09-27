263041c0bcde7f4b50fd9d4bed788644023c1dec
Pijus Pauliukonis <pijus.pauliukonis@oxylabs.io>
2021-09-27 19:56:51 +0300

    Added menu for snowflakes

--- src/Component/Menu/Menu.tsx
+++ src/Component/Menu/Menu.tsx
@@ -1,19 +1,33 @@
-import React, { Fragment } from 'react';
-import { Link } from 'react-router-dom';
-import { Button } from '@material-ui/core';
-import {SNOWFLAKE_BROWNIAN, SNOWFLAKE_KOCH} from '../../config/routes';
+import React, { Fragment } from 'react'
+import { Button, MenuItem, Menu as MuiMenu } from '@material-ui/core'
+import { SNOWFLAKE_BROWNIAN, SNOWFLAKE_KOCH } from '../../config/routes'
 
 const Menu: React.FC = () => {
-    return (
-        <Fragment>
-                <Link to={SNOWFLAKE_BROWNIAN.PATH} >
-                    <Button>Brownian</Button>
-                </Link>
-            <Link to={SNOWFLAKE_KOCH.PATH} >
-                    <Button>Koch</Button>
-                </Link>
-        </Fragment>
-    );
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const open = Boolean(anchorEl)
+
+  const handleClick = (event: any) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  return (
+    <Fragment>
+      <Button onClick={handleClick}>
+        Snowflakes
+      </Button>
+      <MuiMenu
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+      >
+        <MenuItem component="a" href={SNOWFLAKE_BROWNIAN.PATH} onClick={handleClose}>Brownian</MenuItem>
+        <MenuItem component="a" href={SNOWFLAKE_KOCH.PATH} onClick={handleClose}>Koch</MenuItem>
+      </MuiMenu>
+    </Fragment>
+  )
 }
 
-export default Menu;
+export default Menu
